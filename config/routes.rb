3ccd64f034f3890808c2cca62233cc2dbd7f2f3a
Gor <7495500@gmail.com>
2021-02-05 13:19:16 +0300

    Add carrierwave gem configure and update Posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :accounts
+
   get '/dashboard' => 'accounts#index'
+
+  resources :posts, only: [:new, :create, :show]
+
   root to: "public#homepage"
 end
